274cfbdd90551b84ce3f383f4457395b0f6be293
Valentina Cirigliano <valentinacirigliano@hotmail.com>
2022-07-11 23:02:04 -0300

    Agregar archivos de proyecto.

---
+++ TrianguloPOOparcial.Consola/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TrianguloPOOparcial.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Triangulo(12, 14, 15);
+            var t2 = new Triangulo(10, 6, 6);
+            var t3 =new Triangulo(4, 5, 10);
+            var t4 = new Triangulo(5, 10, 11);
+            MostrarAnalisis(t1);
+            MostrarAnalisis(t2);
+            MostrarAnalisis(t3);
+            MostrarAnalisis(t4);
+            
+            
+        }
+
+        private static void MostrarAnalisis(Triangulo t)
+        {
+            if (t.EsTriangulo())
+            {
+                Console.WriteLine("El perimetro es de "+t.GetPerimetro());
+                Console.WriteLine("La superficie es de "+t.GetSuperficie());
+                Console.WriteLine("Es un triángulo de tipo "+t.GetTipoTriangulo());
+            }
+            else
+            {
+                Console.WriteLine("No es un triángulo ");
+            }
+
+            Console.WriteLine("-----------------");
+        }
+    }
+}
---
+++ TrianguloPOOparcial/Triangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace TrianguloPOOparcial
+{
+    public  class Triangulo
+    {
+        private  int LadoA { get; set; }
+        private  int LadoB { get; set; }
+        private  int LadoC { get; set; }
+        public  Triangulo(int ladoA, int ladoB,int ladoC)
+        {
+            LadoA = ladoA;
+            LadoB = ladoB;
+            LadoC = ladoC;
+        }
+
+        public Triangulo() : this(1, 1, 1)
+        {
+
+        }
+
+        public int GetLadoA() => this.LadoA;
+        public  int GetLadoB() => this.LadoB;
+        public  int GetLadoC() => this.LadoC;
+
+        public bool EsTriangulo()
+        {
+            return (((LadoA + LadoB) > LadoC) && ((LadoA + LadoC) > LadoB) && ((LadoB + LadoC) > LadoA));
+        }
+
+        public string GetTipoTriangulo()
+        {
+            if (Math.Pow(LadoA, 2) == LadoB + LadoC)
+            {
+                return "Rectángulo";
+            }else if (Math.Pow(LadoA, 2) > (LadoB + LadoC))
+            {
+                return "Obtusángulo";
+            }
+            else
+            {
+                return "Acutángulo";
+            }
+        }
+        public double GetSuperficie()
+        {
+            double p = (LadoA + LadoB + LadoC) / 2;
+            return Math.Sqrt(p * (p - LadoA) * (p - LadoB) * (p - LadoC));
+
+        }
+
+        public double GetPerimetro()
+        {
+            return LadoA + LadoB + LadoC;
+        }
+    }
+}
